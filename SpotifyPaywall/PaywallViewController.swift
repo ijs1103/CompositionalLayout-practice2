a93c66b7a6fc8afa84250dce552be166f498a262
ijs1103 <ajaijs1103@naver.com>
2023-01-13 14:33:04 +0900

    Initial Commit

---
+++ SpotifyPaywall/PaywallViewController.swift
@@ -0,0 +1,75 @@
+//
+//  PaywallViewController.swift
+//  SpotifyPaywall
+//
+//  Created by joonwon lee on 2022/04/30.
+//
+
+import UIKit
+
+// https://developer.spotify.com/documentation/general/design-and-branding/#using-our-logo
+// https://developer.apple.com/documentation/uikit/nscollectionlayoutsectionvisibleitemsinvalidationhandler
+// 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
+
+class PaywallViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let bannerInfos: [BannerInfo] = BannerInfo.list
+    let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
+    
+    enum Section {
+        case main
+    }
+    typealias Item = BannerInfo
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Presentation
+        datasource = UICollectionViewDiffableDataSource<Section, Item>.init(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
+                return nil
+            }
+            cell.configure(item)
+            cell.backgroundColor = self.colors[indexPath.item]
+            return cell
+        })
+        
+        // Data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(bannerInfos, toSection: .main)
+        datasource.apply(snapshot)
+        
+        // layout
+        collectionView.collectionViewLayout = layout()
+        collectionView.alwaysBounceVertical = false
+        
+        self.pageControl.numberOfPages = bannerInfos.count
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        // itemSize는 groupSize를 따라가게 하기 위해, fraction을 1로줌
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        // section을 item을 하나씩 보여주는 Carousel 형태로 배치
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = 20
+        // 현재 보여지고 있는 화면에 어떤 항목이 표시되는지 알 수 있는 api
+        section.visibleItemsInvalidationHandler = { (items, offset, env) in
+            let index = Int((offset.x / env.container.contentSize.width).rounded(.up))
+            print("--> \(index)")
+            self.pageControl.currentPage = index
+        }
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
